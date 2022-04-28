c1a18b6cf9a5ed0b629138f31a9403ec518bf604
Geryon04 <juanjavierzerofour@gmail.com>
2022-04-28 17:04:45 -0300

    Bandera draw

---
+++ Bandera.js
@@ -0,0 +1,7 @@
+var pantalla = document.querySelector("canvas");
+var pincel = pantalla.getContext("2d");
+
+//Celeste
+pincel.fillStyle = "blue";
+pincel.fillRect(0, 0, 600, 100);
+pincel.fillRect(0, 300, 600, 100);
