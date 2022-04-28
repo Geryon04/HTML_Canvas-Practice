d9fafd9c8e429b2a0bb7e0b6cb3b072299ffa5b8
Geryon04 <juanjavierzerofour@gmail.com>
2022-04-28 17:17:29 -0300

    strokes draw

--- Bandera.js
+++ Bandera.js
@@ -5,3 +5,7 @@ var pincel = pantalla.getContext("2d");
 pincel.fillStyle = "blue";
 pincel.fillRect(0, 0, 600, 100);
 pincel.fillRect(0, 300, 600, 100);
+
+//cuadricula
+pincel.strokeStyle = "black";
+pincel.strokeRect(0, 0, 600, 400);
