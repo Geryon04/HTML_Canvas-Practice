40b5e3e67acb80ac7924e30adea329961e586466
Geryon04 <juanjavierzerofour@gmail.com>
2022-04-27 21:30:36 -0300

    Escuadra draw

--- Escuadra.js
+++ Escuadra.js
@@ -1,8 +1,22 @@
 var pantalla = document.querySelector("canvas");
 var pincel = pantalla.getContext("2d");
 
+//Black
+pincel.fillStyle = "black";
 pincel.beginPath();
 pincel.moveTo(50, 50);
 pincel.lineTo(50, 400);
-
+pincel.lineTo(400, 400);
 pincel.fill();
+
+//White
+pincel.fillStyle = "white";
+pincel.beginPath();
+pincel.moveTo(100, 175);
+pincel.lineTo(100, 350);
+pincel.lineTo(275,350);
+pincel.fill();
+
+pincel.beginPath();
+pincel.moveTo(50, 50);
+pincel.lineTo(50, 400);
