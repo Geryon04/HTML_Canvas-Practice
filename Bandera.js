2f41886bd1b2d265411fe27100dcc5b67d15de2a
Geryon04 <juanjavierzerofour@gmail.com>
2022-04-29 16:24:47 -0300

    bandera draw

--- Bandera.js
+++ Bandera.js
@@ -9,3 +9,7 @@ pincel.fillRect(0, 300, 600, 100);
 //cuadricula
 pincel.strokeStyle = "black";
 pincel.strokeRect(0, 0, 600, 400);
+
+//circulo
+pincel.fillStyle = "Yellow";
+pincel.arc(300, 200, 20, 0, 2*3.14);
