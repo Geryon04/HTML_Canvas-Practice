15c11e0b9e2207c7a889ecb19b41f6e1819b1f1a
Geryon04 <juanjavierzerofour@gmail.com>
2022-04-29 21:17:45 -0300

    flower draw

--- Flower.js
+++ Flower.js
@@ -10,3 +10,9 @@ function dibujarCirculo(x, y, radio, color){
     pincel.arc(x, y, radio, 0, 2*3.14);
     pincel.fill();
 }
+
+dibujarCirculo(300, 220, 10, "blue");
+dibujarCirculo(300, 200, 10, "red");
+dibujarCirculo(300, 180, 10, "yellow");
+dibujarCirculo(280, 200, 10, "orange");
+dibujarCirculo(320, 200, 10, "black");
