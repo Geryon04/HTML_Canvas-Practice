3add462f52d1bfa01932aba05a2420d4b0b823da
Geryon04 <juanjavierzerofour@gmail.com>
2022-04-30 19:06:47 -0300

    Graphic draw

--- Grafico_de_Barra.js
+++ Grafico_de_Barra.js
@@ -1,3 +1,7 @@
+var serie2009 = [6, 47, 41, 3, 3];
+var serie2019 = [81, 9, 3, 3, 4];
+var colores = ["blue", "green", "yellow", "red", "gray"];
+
 function dibujarRectangulo(x, y, base, altura, color){
     var pantalla = document.querySelector("canvas");
     var pincel = pantalla.getContext("2d");
@@ -16,3 +20,10 @@ function escribirTexto(x, y, texto){
     pincel.fillStyle = "black";
     pincel.fillText(texto, x, y);
 }
+
+function dibujarBarra(x, y, serie, colores, texto){
+
+}
+
+dibujarBarra(50, 50, serie2009, colores, "2009");
+dibujarBarra(150, 50, serie2019, colores, "2019");
