36f6c2c1a5f1a92647b4d6e098848ba73d823d03
Geryon04 <juanjavierzerofour@gmail.com>
2022-05-04 17:31:22 -0300

    selector implementation

--- Selector.js
+++ Selector.js
@@ -3,3 +3,6 @@ var selector = document.querySelector("input");
 if(selector.value == "flower"){
     document.write("<script src=\"Flower.js\"></script>");
 }
+if(selector.value == "grafico"){
+    document.write("<script src=\"Flower.js\"></script>");
+}
