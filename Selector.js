c3a13032c7e0b243699e1cc0e313b32418776d59
Geryon04 <juanjavierzerofour@gmail.com>
2022-05-05 18:05:53 -0300

    selector implementation

--- Selector.js
+++ Selector.js
@@ -4,5 +4,9 @@ if(selector.value == "flower"){
     document.write("<script src=\"Flower.js\"></script>");
 }
 if(selector.value == "grafico"){
-    document.write("<script src=\"Flower.js\"></script>");
+    document.write("<script src=\"Grafico_de_Barra.js\"></script>");
+}
+
+if(selector.value == "bandera"){
+    document.write("<script src=\"Bandera.js\"></script>");
 }
