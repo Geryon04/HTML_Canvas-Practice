b1d9cbcb8aa9d4c5537ab3b74533d15f6ff50dff
Geryon04 <juanjavierzerofour@gmail.com>
2022-05-05 19:09:29 -0300

    point draw

--- Point_Draw.js
+++ Point_Draw.js
@@ -3,12 +3,14 @@ var pincel = pantalla.getContext("2d");
 
 pincel.fillStyle = "grey"
 pincel.fillRect(0, 0, 600, 400);
+var colores = ["blue", "red", "green"];
+var indiceColorActual = 0;
 
 function dibujarCirculo(evento){
     var x = evento.pageX - pantalla.offsetLeft;
     var y = evento.pageY - pantalla.offsetTop;
 
-    pincel.fillStyle = "blue"
+    pincel.fillStyle = colores[indiceColorActual];
     pincel.beginPath();
     pincel.arc(x, y, 10, 0, 2*3.14);
     pincel.fill();
