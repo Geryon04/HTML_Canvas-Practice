3b4d111af3f1d422d4a8c11405ea4960c6513eb4
Geryon04 <juanjavierzerofour@gmail.com>
2022-05-11 17:39:55 -0300

    brocha animation

--- Selector.js
+++ Selector.js
@@ -1,16 +1,5 @@
 var selector = document.querySelector("input");
 
-if(selector.value == "flower"){
-    document.write("<script src=\"Flower.js\"></script>");
-}
-if(selector.value == "grafico"){
-    document.write("<script src=\"Grafico_de_Barra.js\"></script>");
-}
-
-if(selector.value == "bandera"){
-    document.write("<script src=\"Bandera.js\"></script>");
-}
-
 if(selector.value == "escuadra"){
     document.write("<script src=\"Escuadra.js\"></script>");
 }
