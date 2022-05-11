c53b18e21bd1014ae37d6563fecfdade66320a2c
Geryon04 <juanjavierzerofour@gmail.com>
2022-05-10 23:41:04 -0300

    brocha animation

--- Selector.js
+++ Selector.js
@@ -14,3 +14,7 @@ if(selector.value == "bandera"){
 if(selector.value == "escuadra"){
     document.write("<script src=\"Escuadra.js\"></script>");
 }
+
+if(selector.value == "brocha"){
+    document.write("<script src=\"Brocha.js\"></script>");
+}
