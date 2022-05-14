5924dc601b1e741bd4f0136811db2748cb064b99
Geryon04 <juanjavierzerofour@gmail.com>
2022-05-14 16:43:54 -0300

    selector implementation

--- Selector.js
+++ Selector.js
@@ -8,11 +8,6 @@ pincel.fillRect(0, 0, 100, 100);
 
 var selector = document.querySelector("input");
 
-
-if(selector.value == "point"){
-    document.write("<script src=\"Point_Draw.js\"></script>");
-}
-
 if(selector.value == "brocha"){
     document.write("<script src=\"Brocha.js\"></script>");
 }
