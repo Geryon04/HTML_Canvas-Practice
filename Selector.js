c6a0b7f9fc08c8e16c0b15f4c65aed4b94258a01
Geryon04 <juanjavierzerofour@gmail.com>
2022-05-15 00:09:37 -0300

    selector implementation

--- Selector.js
+++ Selector.js
@@ -1,13 +1,20 @@
 var pantalla = document.querySelector("canvas");
 var pincel = pantalla.getContext("2d");
+var selector = document.querySelector("input");
+var boton = document.querySelector("button");
+var comando = "";
 
+function ingresarComando(){
+
+}
 
 pincel.fillStyle = "grey";
 pincel.fillRect(0, 0, 100, 100);
 
 
-var selector = document.querySelector("input");
+
 
 if(selector.value == "brocha"){
     document.write("<script src=\"Brocha.js\"></script>");
+    console.log(evento);
 }
