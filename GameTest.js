1b040f33c1b04d2eeca5d92f67cfc01c5efd25ab
Geryon04 <juanjavierzerofour@gmail.com>
2022-05-25 19:24:13 -0300

    game test

--- GameTest.js
+++ GameTest.js
@@ -1,17 +1,6 @@
 var pantalla = document.querySelector("canvas");
 var pincel = pantalla.getContext("2d");
 
-//background
-pincel.fillStyle = "rgb(100, 150, 200)";
-pincel.fillRect(0, 0, 600, 400);
-
-//floor
-pincel.fillStyle = "rgb(0, 0, 0)";
-pincel.fillRect(0, 350, 600, 50);
-
-//plataform test
-pincel.fillRect(200, 250, 100, 30);
-
 //arrow key codes
 var UP = 38;
 var DOWN = 40;
@@ -19,8 +8,21 @@ var RIGHT = 39;
 var LEFT = 37;
 
 //position
-var posX = 0;
+var posX = 20;
 var posY = 150;
+
+function scene() {
+    //background
+    pincel.fillStyle = "rgb(100, 150, 200)";
+    pincel.fillRect(0, 0, 600, 400);
+
+    //floor
+    pincel.fillStyle = "rgb(0, 0, 0)";
+    pincel.fillRect(0, 350, 600, 50);
+
+    //plataform test
+    pincel.fillRect(200, 250, 100, 30);
+}
 
 //Character test
 function character(x, y) {
@@ -32,14 +34,18 @@ function character(x, y) {
 //update screen
 function clearScreen() {
     pincel.clearRect(0, 0, 600, 400);
+    scene();
 }
 
 //character movement
-function movement(x, y) {
-    while(x <= 300){
-        x = x + 5;
+function movement() {
+    clearScreen();
+    var d = new Date();
+    if (posX <= 400) {
+        character(posX, posY);
+        posX = posX + 1;
+        console.log(posX + " " + posY);
     }
-    character(x, y);
 }
-setInterval(movement(posX, posY), 50);
+setInterval(movement, 50);
 
